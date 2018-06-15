8ea11e9a81c1ba126b94608866feecf1d5cba9de
dscudeveloper <37447475+dscudeveloper@users.noreply.github.com>
2018-06-15 16:05:47 -0400

    2018-06-15

---
+++ java_pi.java
@@ -0,0 +1,30 @@
+import java.lang.Math;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.commons.io.*;
+public class java_pi {
+
+	public static void main(String[] args) throws InvalidPasswordException, IOException {
+		// TODO Auto-generated method stub
+        int k;
+        double acc = 0.0;
+        File file = new File("U:\\Open SIMS\\Data for Open SIMS\\Application and Confirmation Data\\OUAC.PDF");
+        PDDocument document = PDDocument.load(file);
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+        String text = pdfStripper.getText(document);
+        document.close();
+        
+        System.out.println(text);
+        for (k=0;k<10000;k++ ) {
+        	acc = acc + Math.pow (-1,k)/(2*k+1);
+        }
+        acc = 4 *acc;
+        System.out.print("pi: ");
+        System.out.print(acc);
+	}
+
+}
